845d5cef1281387056f46ada3250dea20f434c99
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-24 21:07:57 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,17 +4,25 @@ import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 config();
 const app = express();
 
-//middlewares
-app.use(cors());
+// CORS configuration
+const corsOptions = {
+  origin: "https://mern-nn78yafqe-raj-24642cc0.vercel.app", // Frontend URL
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+// Middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-//remove it in production
+// Remove it in production
 app.use(morgan("dev"));
 
+// API routes
 app.use("/api/v1", appRouter);
 
 export default app;
